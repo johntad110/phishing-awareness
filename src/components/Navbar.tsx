716011145e63b2995f234ba108f189b1c9939006
John Tad <johntad110@gmail.com>
2024-06-28 16:47:44 +0300

    add github link to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,16 @@
+import { FaGithub } from "react-icons/fa6";
+
 function Navbar() {
     return (
-        <div className="bg-white py-4 px-4 border-b border-black">
+        <div className="bg-white py-4 px-4 md:px-10 lg:px-24 border-b border-black flex items-center justify-between">
             <p className="text-black font-bold text-xl">Phishing Awareness</p>
+            <div
+                className="hidden sm:flex gap-2 border border-black p-2 rounded-lg hover:bg-black hover:text-white hover:cursor-pointer hover:rounded-none transition-all"
+                onClick={() => document.location.href = "https://github.com/johntad110/phishing-awareness"}
+            >
+                <p className="">Github</p>
+                <FaGithub className="text-2xl" />
+            </div>
         </div>
     )
 }
