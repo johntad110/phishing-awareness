f447e8173633bb9256c99cdf40193f1e289de147
M1CK3YM <michaelyirdaw652@gmail.com>
2024-06-29 19:46:01 +0300

    edit questionnaire and add questions.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import Status from './components/Status'
-import FormInput from './components/FormInput'
-import ResultsDisplay from './components/ResultsDisplay'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Status from "./components/Status";
+import FormInput from "./components/FormInput";
+import ResultsDisplay from "./components/ResultsDisplay";
+import Questionnaire from "./components/Questionnaire";
+import qna from "./data/questions.json";
+
+export interface QnA {
+  question: String,
+  choice: String[],
+  answer: number
+}
 
 function App() {
   const [showForm, setShowForm] = useState(true);
   const [showQuestions, setShowQuestions] = useState(false);
   const [showResults, setShowResults] = useState(false);
+  const QnAs: QnA[] = qna.map((value) => ({ value, sort: Math.random() }))
+    .sort((a, b) => (a.sort - b.sort))
+    .map(({ value }) => ({
+      question: value.question,
+      choice: value.choice,
+      answer: value.answer
+    }));
 
   const handleStart = () => {
     setShowForm(false);
@@ -24,24 +39,27 @@ function App() {
     setShowForm(true);
     setShowQuestions(false);
     setShowResults(false);
-  }
+  };
 
   return (
-    <div className='min-h-screen'>
+    <div className="min-h-screen">
       <Navbar />
-      <div className='flex flex-col items-center'>
+      <div className="flex flex-col items-center">
         {showForm && <FormInput onStart={handleStart} />}
         {showQuestions && (
           <>
-            <Status currentQuestion={5} totalQuestions={8} onRestart={handleRestart} />
-            {/* Add Questionnaire component here. */}
-            <h2 className='mt-8'>Questionnaire Component Placeholder</h2>
+            <Status
+              currentQuestion={5}
+              totalQuestions={8}
+              onRestart={handleRestart}
+            />
+            {<Questionnaire QnAs={QnAs}/>}
           </>
         )}
         {showResults && <ResultsDisplay />}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Questionnaire.tsx
+++ src/components/Questionnaire.tsx
@@ -1,6 +1,24 @@
-export default function Questionnaire() {
-    return (
-        <div></div>
-    )
-    
+import { QnA } from "../App";
+
+type QProps = {
+    QnAs: QnA[];
 }
+
+const Questionnaire: React.FC<QProps> = ({ QnAs }) => {
+    
+    console.log(QnAs[0].question);
+    return (
+        <div className="bg-gray-200 w-[80%] md:min-w-[600px] min-h-96 rounded-es-2xl rounded-ee-2xl mt-6 p-9 flex flex-col justify-center align-middle">
+            <div className="bg-transparent w-[70%] md:min-w-[400px] min-h-80 m-auto">
+                
+            </div>
+            <div className="m-auto flex justify-around align-middle space-x-24 w-[60%]">
+                <button className="bg-black text-white py-4 px-16 border border-black hover:bg-white hover:text-black transition-all">CLEAR</button>
+                <button className="bg-black text-white py-4 px-16 border border-black hover:bg-white hover:text-black transition-all">SUBMIT</button>
+            </div>
+        </div>
+    )
+
+}
+
+export default Questionnaire;
