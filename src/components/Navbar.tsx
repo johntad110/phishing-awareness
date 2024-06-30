c96abcf719bbfd4c9f0679b865b672b066d5b730
John Tad <johntad110@gmail.com>
2024-06-30 09:34:22 +0300

    remove roundedness for uniformity

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
         <div className="bg-white py-4 px-4 md:px-10 lg:px-24 border-b border-black flex items-center justify-between">
             <p className="text-black font-bold text-xl">Phishing Awareness</p>
             <div
-                className="hidden sm:flex gap-2 border border-black p-2 rounded-lg hover:bg-black hover:text-white hover:cursor-pointer hover:rounded-none transition-all"
+                className="hidden sm:flex gap-2 border border-black p-2 hover:bg-black hover:text-white hover:cursor-pointer transition-all"
                 onClick={() => document.location.href = "https://github.com/johntad110/phishing-awareness"}
             >
                 <p className="">Github</p>
